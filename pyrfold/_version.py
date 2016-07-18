da86048468b45501a153eed17b54456047bbc4ff
wmvoje <williamvoje@gmail.com>
2016-07-18 16:56:11 -0700

    Resolving merg issue

--- pyrfold/_version.py
+++ pyrfold/_version.py
@@ -1,5 +1,9 @@
 """
 Version and changelog of pyrfold
+
+Version 0.2.1 optmized hyak processing as it stood in 0.2.0.
+Minor update
+
 """
 
-__version__ = "0.2.0_device_update_2"
+__version__ = "0.2.1"

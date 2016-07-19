313c313d0e4269a470a4153abecf0678890fbacc
Willy <williamvoje@gmail.com>
2016-07-18 17:19:08 -0700

    Update _version.py

--- pyrfold/_version.py
+++ pyrfold/_version.py
@@ -3,7 +3,9 @@ Version and changelog of pyrfold
 
 Version 0.2.1 optmized hyak processing as it stood in 0.2.0.
 Minor update
+Version 0.2.2 added fold.vienna.mfe_plus() function
+Minor update
 
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
